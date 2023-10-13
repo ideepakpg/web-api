48e1e43beac1d1258f983ba3a140f5092e03e5d7
Deepak PG <deepakpg121@gmail.com>
2023-10-13 15:16:03 +0530

    Create AppDbContext class

---
+++ Model/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace web_api.Model
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+        public DbSet<Member> Members { get; set; }
+    }
+}
