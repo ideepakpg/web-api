a1aff495e101c892a00b4004eb2190a059003639
Deepak PG <deepakpg121@gmail.com>
2023-10-13 15:12:08 +0530

    Configure DbContext for SQL Server

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using web_api.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DataContextConnection"), o =>
+    {
+        o.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+    }));
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
